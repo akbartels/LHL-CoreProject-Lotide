4f6d63d3a3a77be8ce8e96488f07adb4206e709b
Amy Bartels <akunrau@gmail.com>
2020-04-01 20:29:06 +0000

    created function to return new array of a single middle item (odd array) or 2 item array (even array)

---
+++ middle.js
@@ -0,0 +1,42 @@
+const eqArrays = function(array1, array2) {
+  if (JSON.stringify(array1) === JSON.stringify(array2)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+
+const assertArraysEqual = function(array1, array2) {
+  let compareArray = eqArrays(array1, array2);
+  
+  if (compareArray) {
+    console.log(`✅ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${array1} !==  ${array2}`);
+  }
+};
+
+// create function named: middle
+// should return a new array with only middle elements  of array
+// length of returned array could vary: odd should give one element, even should give two
+// need to determine length of array
+// 
+
+
+const middle = function(array) {
+  let result = [];
+  let length = array.length;
+  let half = length / 2;
+    
+  if (length % 2 === 0) {
+    result.push(array[half-1], array[half]);
+  } else {
+    result.push(array[Math.floor(half)]);
+  }
+  return result;
+};
+
+let arrayTest = [1, "b", "c", 4, 5];
+
+assertArraysEqual(middle(arrayTest), ["c"])
