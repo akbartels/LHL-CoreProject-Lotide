f9e90a8e52f5c73ec84bb718331beaa7050ddac8
Amy Bartels <akunrau@gmail.com>
2020-04-02 01:44:22 +0000

    Justin showed me how to use module to pull my function into another .js file

--- assertEqual.js
+++ assertEqual.js
@@ -6,8 +6,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(1, 6);
-assertEqual("amy", "amy");
-
+module.exports = assertEqual;
