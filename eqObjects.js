d29263a662d900899a01be952a2fda12ec1422dd
Amy Bartels <akunrau@gmail.com>
2020-04-05 23:50:20 +0000

    was really confused with this one, so wanted to completely re-do it to practice it. This is my newer result

--- eqObjects.js
+++ eqObjects.js
@@ -21,30 +21,30 @@ const eqArrays = function(array1, array2) {
 // object equal when value for each key is the same as same key value in other object
 
 const eqObjects = function(object1, object2) {
-  let keysOb1 = Object.keys(object1)
-  let keysOb2 = Object.keys(object2)
-
+  const keysOb1 = Object.keys(object1); 
+    //turns into array of stringed keys: ["a", "b"]
+  const keysOb2 = Object.keys(object2);
+ 
   if (keysOb1.length === keysOb2.length) {
-    for (let key of keysOb1){
-      if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-        if (!(eqArrays(object1[key], object2[key]))) {
+    for (let key of keysOb1) {
+      if (Array.isArray(object1[key]) === Array.isArray(object2[key])) {
+        if (eqArrays(object1[key], object2[key]) === false) {
           return false;
-        };
-      }else if (keysOb1[key] !== keysOb2[key]) {
-        return false;
+        }
       } 
     }
     return true;
-  } 
+  }
   return false;
-};
+}
 
-// const ab = { a: "1", b: "2" };
-// const ba = { b: "2", a: "1" };
-// assertEqual(eqObjects(ab, ba), true); // => true
 
-// const abc = { a: "1", b: "2", c: "3" };
-// assertEqual(eqObjects(ab, abc), false); // => false
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+assertEqual(eqObjects(ab, ba), true); // => true
+
+const abc = { a: "1", b: "2", c: "3" };
+assertEqual(eqObjects(ab, abc), false); // => false
 
 
 const cd = { c: "1", d: ["2", 3]};
@@ -53,3 +53,5 @@ assertEqual(eqObjects(cd, dc), true); //
 
 const cd2 = { c: "1", d: ["2", 3, 4] };
 assertEqual(eqObjects(cd, cd2), false); // => false
+
+// console.log(eqObjects(cd, dc))
